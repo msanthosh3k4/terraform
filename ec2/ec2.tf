2047f25122a65b9f4a80db74c1b1c13b03dabe09
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-10-24 22:44:10 +0530

    Terraform-multiple-env-practice

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
-    default = "Expense-dev"
+    Name = "Expense-dev"
   }
 }
 
--- for-each/providers.tf
+++ for-each/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+    backend "s3" {
+      bucket = "82s-terra-remote-state"
+      key    = "expense-infra" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+      region = "us-east-1"
+      dynamodb_table = "82s-state-locking"
+  }
 }
 
 # Configure the AWS Provider
